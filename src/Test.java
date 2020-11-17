7442bf5fc3ce3fdba6a5a64f974653909610fe6d
unknown <loujinming1998@163.com>
2020-11-17 18:00:20 +0800

    2020.11.17

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,9 @@
 public class Test {
+    public static void main(String[] args) {
+        //三木运算经典套娃
+        System.out.println(true?false:true?false:true);
+        System.out.println(false?false:true?false:true);
+        //取反，取的时候作为补码处理
+        System.out.println(~4);
+    }
 }
