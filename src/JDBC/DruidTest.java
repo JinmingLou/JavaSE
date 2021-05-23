ed7ce983cb842bf274b6ac638592d5910b73135b
JinmingLou <975710389@qq.com>
2021-05-23 14:09:37 +0800

    “2021.5.23”

---
+++ src/JDBC/DruidTest.java
@@ -0,0 +1,23 @@
+package JDBC;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class DruidTest {
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream("src/JDBC/druid.properties");
+        properties.load(fileInputStream);
+
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(properties);
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+}
